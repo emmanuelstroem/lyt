4e0fa8572db9909f3b4136fc11e02278df34278f
Emmanuel <1090777+emmanuelstroem@users.noreply.github.com>
2025-07-24 13:38:56 +0200

    add: xcode project initialization
    Signed-off-by: Emmanuel <1090777+emmanuelstroem@users.noreply.github.com>

---
+++ lyt/lytApp.swift
@@ -0,0 +1,17 @@
+//
+//  lytApp.swift
+//  lyt
+//
+//  Created by Emmanuel on 24/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct lytApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lytTests/lytTests.swift
@@ -0,0 +1,16 @@
+//
+//  lytTests.swift
+//  lytTests
+//
+//  Created by Emmanuel on 24/07/2025.
+//
+
+import Testing
+
+struct lytTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
